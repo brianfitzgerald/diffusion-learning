85e8feb9918d5736f231f07743fba1f0d35a0213
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2023-06-24 15:02:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Diffusion Learning
+
+Minimal re-implementation of DDIM for learning purposes.
+
+### References
+
+- https://huggingface.co/blog/annotated-diffusion
+- https://github.com/lucidrains/denoising-diffusion-pytorch
+- https://github.com/huggingface/diffusion-models-class/blob/main/unit1/02_diffusion_models_from_scratch.ipynb
+- https://github.com/huggingface/diffusion-models-class/tree/main/unit2 - for guidance and fine tuning
---
+++ model.py
@@ -0,0 +1,212 @@
+from inspect import isfunction
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+from inspect import isfunction
+from functools import partial
+
+from tqdm.auto import tqdm
+from einops import rearrange, reduce
+from einops.layers.torch import Rearrange
+
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+
+
+def exists(x):
+    return x is not None
+
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+
+def num_to_groups(num, divisor):
+    groups = num // divisor
+    remainder = num % divisor
+    arr = [divisor] * groups
+    if remainder > 0:
+        arr.append(remainder)
+    return arr
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, *args, **kwargs):
+        return self.fn(x, *args, **kwargs) + x
+
+
+def Upsample(dim, dim_out=None):
+    return nn.Sequential(
+        nn.Upsample(scale_factor=2, mode="nearest"),
+        nn.Conv2d(dim, default(dim_out, dim), 3, padding=1),
+    )
+
+
+def Downsample(dim, dim_out=None):
+    # rearrange -> conv2d with stride 2 is equivalent to downsample -> rearrange
+    # https://arxiv.org/abs/2208.03641
+    # Space to depth layer - i.e. instead of striding, we rearrange the input tensor
+    # into a different shape, and then apply a convolutional layer with stride 1
+    # Pooling layers result in feature loss, so instead we split the map to channels
+    # and then apply a convolutional layer with stride 1 to each channel
+    return nn.Sequential(
+        Rearrange("b c (h p1) (w p2) -> b (c p1 p2) h w", p1=2, p2=2),
+        nn.Conv2d(dim, default(dim_out, dim), 3, stride=2, padding=1),
+    )
+
+
+# Input - batch of noise levels, and converts to an embedding
+# This is inspired by Transformer networks!
+# interleaving sin,cos
+# Idea is to decrease the siusoid frequency as we go deeper into position
+# this represents the higher values that the rotational encoding represents
+# https://kazemnejad.com/blog/transformer_architecture_positional_encoding/
+class SinusoidalPositionalEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+
+
+class WeightStandardizedConv2d(nn.Conv2d):
+    """
+    https://arxiv.org/abs/1903.10520
+    Weight standardization is a simple reparameterization of the weights
+    """
+
+    def forward(self, x):
+        eps = 1e-5 if x.dtype == torch.float32 else 1e-3
+
+        weight = self.weight
+        # this is the weight standardization
+        mean = reduce(weight, "o ... -> o 1 1 1", "mean")
+        var = reduce(weight, "o ... -> o 1 1 1", partial(torch.var, unbiased=False))
+        # rsqrt is the reciprocal square root
+        normalized_weight = (weight - mean) / (var + eps).rsqrt()
+
+        return F.conv2d(
+            x,
+            normalized_weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups,
+        )
+
+
+# weight standardized wide conv2d block
+class Block(nn.Module):
+    def __init__(self, dim, dim_out, groups=8):
+        super().__init__()
+        self.proj = WeightStandardizedConv2d(dim, dim_out, 3, padding=1)
+        self.norm = nn.GroupNorm(groups, dim_out)
+
+        # Element wise sigmoid linear unit
+        self.act = nn.SiLU()
+
+    def forward(self, x, scale_shift=None):
+        x = self.proj(x)
+        x = self.norm(x)
+
+        if exists(scale_shift):
+            scale, shift = scale_shift
+            x = x * scale + shift
+
+        x = self.act(x)
+        return x
+
+
+# https://arxiv.org/abs/1512.03385
+# Just your standard Resnet block.
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, dim_out, *, time_emb_dim=None, groups=8):
+        super().__init__()
+
+        # Convert time embedding to a linear layer
+        self.mlp = (
+            nn.Sequential(nn.SiLU(), nn.Linear(time_emb_dim, dim_out * 2))
+            if exists(time_emb_dim)
+            else None
+        )
+
+        self.block1 = Block(dim, dim_out, groups=groups)
+        self.block2 = Block(dim_out, dim_out, groups=groups)
+
+        # If this is not a residual block, then we need to project the input
+        self.res_conv = nn.Conv2d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
+
+    # Take the time embedding, rearrange, then perform standardization on first layer,
+    # then second layer + add residual
+    def forward(self, x, time_emb=None):
+        scale_shift = None
+        if exists(self.mlp) and exists(time_emb):
+            scale_shift = self.mlp(time_emb)
+            # unsequeeze twice
+            time_emb = self.mlp(time_emb)
+            time_emb = rearrange(time_emb, "b c -> b c 1 1")
+            scale_shift = time_emb.chunk(2, dim=1)
+
+        h = self.block1(x, scale_shift=scale_shift)
+        h = self.block2(h)
+        return h + self.res_conv(x)
+
+
+# multi head attention - but with convolutions
+# first we project the input to qkv, then we split the channels into heads
+# https://github.com/lucidrains/linear-attention-transformer
+# https://arxiv.org/abs/1812.01243
+class Attention(nn.Module):
+    def __init__(self, dim, heads=4, dim_head=32) -> None:
+        super().__init__()
+        self.scale = dim_head**-0.5
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        # each channel is split into heads
+        # each head is projected to qkv
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias=False)
+        self.to_out = nn.Conv2d(hidden_dim, dim, 1)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        q, k, v = map(
+            lambda t: rearrange(t, "b (h c) x y -> b h c (x y)", h=self.heads), qkv
+        )
+        q = q * self.scale
+
+        sim = einsum("b h d i, b h d j -> b h i j", q, k)
+        sim = sim - sim.amax(dim=-1, keepdim=True).detach()
+        attn = sim.softmax(dim=-1)
+
+        out = einsum("b h i j, b h d j -> b h i d", attn, v)
+        out = rearrange(out, "b h (x y) d -> b (h d) x y", x=h, y=w)
+        return self.to_out(out)
+
+# Group normalization - normalize a group of channels
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.fn = fn
+        self.norm = nn.GroupNorm(1, dim)
+
+    def forward(self, x):
+        x = self.norm(x)
+        return self.fn(x)
+
