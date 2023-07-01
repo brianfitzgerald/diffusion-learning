a08a62b2a8d0d415432161f3d4ee04bf3106285e
Brian <cooldude242@gmail.com>
2023-07-01 22:39:57 +0000

    cleanup and refactor

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
-from torchvision import transforms
 from torch.utils.data import DataLoader
-from torchvision.transforms import Compose, ToTensor, Lambda, ToPILImage, CenterCrop, Resize
 from datasets import load_dataset
 from pathlib import Path
 import torch
 from layers import num_to_groups
 from train_utils import *
 from unet import *
+from torchvision.utils import save_image
+from torch.optim import Adam
 
 # load dataset from the hub
 dataset = load_dataset("fashion_mnist")
@@ -15,38 +15,44 @@ channels = 1
 batch_size = 512
 
 
-# define image transformations (e.g. using torchvision)
-transform = Compose([
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Lambda(lambda t: (t * 2) - 1)
-])
+inference_transform, reverse_transform, ds_transform = get_transforms(128)
+
 
 # define function
-def transforms(examples):
-   examples["pixel_values"] = [transform(image.convert("L")) for image in examples["image"]]
-   del examples["image"]
+def dataset_transform_batch(examples):
+    examples["pixel_values"] = [
+        ds_transform(image.convert("L")) for image in examples["image"]
+    ]
+    del examples["image"]
 
-   return examples
+    return examples
 
-transformed_dataset = dataset.with_transform(transforms).remove_columns("label")
+
+transformed_dataset = dataset.with_transform(dataset_transform_batch).remove_columns(
+    "label"
+)
 
 # create dataloader
-dataloader = DataLoader(transformed_dataset["train"], batch_size=batch_size, shuffle=True)
+dataloader = DataLoader(
+    transformed_dataset["train"], batch_size=batch_size, shuffle=True
+)
 
 
 results_folder = Path("./results")
-results_folder.mkdir(exist_ok = True)
+results_folder.mkdir(exist_ok=True)
 save_and_sample_every = 100
 
-from torch.optim import Adam
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 model = Unet(
     dim=image_size,
     channels=channels,
-    dim_mults=(1, 2, 4,)
+    dim_mults=(
+        1,
+        2,
+        4,
+    ),
 )
 model.to(device)
 
@@ -54,37 +60,46 @@ print("using", device)
 
 optimizer = Adam(model.parameters(), lr=1e-3)
 
-from torchvision.utils import save_image
+scheduler = get_schedule(100)
 
 epochs = 100
-timesteps = 100
 
 for epoch in range(epochs):
     print(f"Epoch {epoch}:")
     for step, batch in enumerate(dataloader):
-      optimizer.zero_grad()
+        optimizer.zero_grad()
 
-      batch_size = batch["pixel_values"].shape[0]
-      batch = batch["pixel_values"].to(device)
+        batch_size = batch["pixel_values"].shape[0]
+        batch = batch["pixel_values"].to(device)
 
-      # Algorithm 1 line 3: sample t uniformally for every example in the batch
-      t = torch.randint(0, timesteps, (batch_size,), device=device).long()
+        # Algorithm 1 line 3: sample t uniformally for every example in the batch
+        t = torch.randint(0, scheduler.timesteps, (batch_size,), device=device).long()
 
-      loss = p_losses(model, batch, t, loss_type="huber")
+        loss = p_losses(model, batch, t, loss_type="huber")
 
-      if step % 20 == 0:
-        print(f"Loss for step {step}:", loss.item())
+        if step % 20 == 0:
+            print(f"Loss for step {step}:", loss.item())
 
-      loss.backward()
-      optimizer.step()
+        loss.backward()
+        optimizer.step()
 
-      # save generated images
-      if step != 0 and step % save_and_sample_every == 0:
-        milestone = step // save_and_sample_every
-        batches = num_to_groups(4, batch_size)
-        all_images_list = list(map(lambda n: sample(model, image_size=128, batch_size=n, channels=channels), batches))
-        all_images = torch.cat(all_images_list, dim=0)
-        all_images = (all_images + 1) * 0.5
-        save_image(all_images, str(results_folder / f'sample-{epoch}-{milestone}.png'), nrow = 6)
-        print(f"Sampled for step {step}")
-
+        # save generated images
+        if step != 0 and step % save_and_sample_every == 0:
+            milestone = step // save_and_sample_every
+            batches = num_to_groups(4, batch_size)
+            all_images_list = list(
+                map(
+                    lambda n: sample(
+                        model, image_size=128, batch_size=n, channels=channels
+                    ),
+                    batches,
+                )
+            )
+            all_images = torch.cat(all_images_list, dim=0)
+            all_images = (all_images + 1) * 0.5
+            save_image(
+                all_images,
+                str(results_folder / f"sample-{epoch}-{milestone}.png"),
+                nrow=6,
+            )
+            print(f"Sampled for step {step}")
--- train_utils.py
+++ train_utils.py
@@ -9,33 +9,19 @@ from torchvision.transforms import (
     ToPILImage,
     CenterCrop,
     Resize,
+    RandomHorizontalFlip,
+    
 )
 import numpy as np
 from tqdm import tqdm
+from dataclasses import dataclass
 
-timesteps = 100
 
 def linear_beta_schedule(timesteps):
     beta_start = 0.0001
     beta_end = 0.02
     return torch.linspace(beta_start, beta_end, timesteps)
 
-
-# define beta schedule
-betas = linear_beta_schedule(timesteps=timesteps)
-
-# define alphas
-alphas = 1.0 - betas
-alphas_cumprod = torch.cumprod(alphas, axis=0)
-alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
-sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
-
-# calculations for diffusion q(x_t | x_{t-1}) and others
-sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
-sqrt_one_minus_alphas_cumprod = torch.sqrt(1.0 - alphas_cumprod)
-
-# calculations for posterior q(x_{t-1} | x_t, x_0)
-posterior_variance = betas * (1.0 - alphas_cumprod_prev) / (1.0 - alphas_cumprod)
 
 def cosine_beta_schedule(timesteps, s=0.008):
     """
@@ -47,7 +33,6 @@ def cosine_beta_schedule(timesteps, s=0.008):
     alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
     betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
     return torch.clip(betas, 0.0001, 0.9999)
-
 
 
 def quadratic_beta_schedule(timesteps):
@@ -63,39 +48,83 @@ def sigmoid_beta_schedule(timesteps):
     return torch.sigmoid(betas) * (beta_end - beta_start) + beta_start
 
 
-
 def extract(a, t, x_shape):
     batch_size = t.shape[0]
     out = a.gather(-1, t.cpu())
     return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
 
 
-image_size = 128
-transform = Compose(
-    [
-        Resize(image_size),
-        CenterCrop(image_size),
-        ToTensor(),  # turn into torch Tensor of shape CHW, divide by 255
-        Lambda(lambda t: (t * 2) - 1),
-    ]
-)
+@dataclass
+class Scheduler:
+    sqrt_recip_alphas: float
+    posterior_variance: float
+    sqrt_one_minus_alphas_cumprod: float
+    sqrt_alphas_cumprod: float
+    timesteps: float
 
-reverse_transform = Compose(
-    [
-        Lambda(lambda t: (t + 1) / 2),
-        Lambda(lambda t: t.permute(1, 2, 0)),  # CHW to HWC
-        Lambda(lambda t: t * 255.0),
-        Lambda(lambda t: t.numpy().astype(np.uint8)),
-        ToPILImage(),
-    ]
-)
+
+def get_schedule(timesteps: int = 100, schedule_type="linear"):
+    # define beta schedule
+    if schedule_type == "linear":
+        betas = linear_beta_schedule(timesteps=timesteps)
+
+    # define alphas
+    alphas = 1.0 - betas
+    alphas_cumprod = torch.cumprod(alphas, axis=0)
+    alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+    sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+
+    # calculations for diffusion q(x_t | x_{t-1}) and others
+    sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+    sqrt_one_minus_alphas_cumprod = torch.sqrt(1.0 - alphas_cumprod)
+
+    # calculations for posterior q(x_{t-1} | x_t, x_0)
+    posterior_variance = betas * (1.0 - alphas_cumprod_prev) / (1.0 - alphas_cumprod)
+    return Scheduler(
+        sqrt_recip_alphas,
+        posterior_variance,
+        sqrt_one_minus_alphas_cumprod,
+        sqrt_alphas_cumprod,
+        timesteps
+    )
+
+
+def get_transforms(image_size: int = 128):
+    inference_transform = Compose(
+        [
+            Resize(image_size),
+            CenterCrop(image_size),
+            ToTensor(),  # turn into torch Tensor of shape CHW, divide by 255
+            Lambda(lambda t: (t * 2) - 1),
+        ]
+    )
+
+    reverse_transform = Compose(
+        [
+            Lambda(lambda t: (t + 1) / 2),
+            Lambda(lambda t: t.permute(1, 2, 0)),  # CHW to HWC
+            Lambda(lambda t: t * 255.0),
+            Lambda(lambda t: t.numpy().astype(np.uint8)),
+            ToPILImage(),
+        ]
+    )
+
+    # define image transformations (e.g. using torchvision)
+    ds_transform = Compose(
+        [
+            RandomHorizontalFlip(),
+            ToTensor(),
+            Lambda(lambda t: (t * 2) - 1),
+        ]
+    )
+    return inference_transform, reverse_transform, ds_transform
 
 
 @torch.no_grad()
-def p_sample(model, x, t, t_index):
-    betas_t = extract(betas, t, x.shape)
-    sqrt_one_minus_alphas_cumprod_t = extract(sqrt_one_minus_alphas_cumprod, t, x.shape)
-    sqrt_recip_alphas_t = extract(sqrt_recip_alphas, t, x.shape)
+def p_sample(model, scheduler: Scheduler, x, t, t_index):
+    betas_t = extract(scheduler.betas, t, x.shape)
+    sqrt_one_minus_alphas_cumprod_t = extract(scheduler.sqrt_one_minus_alphas_cumprod, t, x.shape)
+    sqrt_recip_alphas_t = extract(scheduler.sqrt_recip_alphas, t, x.shape)
 
     # Equation 11 in the paper
     # Use our model (noise predictor) to predict the mean
@@ -106,15 +135,16 @@ def p_sample(model, x, t, t_index):
     if t_index == 0:
         return model_mean
     else:
-        posterior_variance_t = extract(posterior_variance, t, x.shape)
+        posterior_variance_t = extract(scheduler.posterior_variance, t, x.shape)
         noise = torch.randn_like(x)
         # Algorithm 2 line 4:
         return model_mean + torch.sqrt(posterior_variance_t) * noise
 
 
 # Algorithm 2 (including returning all images)
+# Sample all timesteps in a loop - used for sampling at inference
 @torch.no_grad()
-def p_sample_loop(model, shape):
+def p_sample_loop(model, shape, timesteps: int):
     device = next(model.parameters()).device
 
     b = shape[0]
@@ -131,8 +161,10 @@ def p_sample_loop(model, shape):
         imgs.append(img.cpu())
     return imgs
 
-
-from pathlib import Path
+@torch.no_grad()
+def sample(model, image_size, batch_size=16, channels=3):
+    res = p_sample_loop(model, shape=(batch_size, channels, image_size, image_size))
+    return res[0]
 
 
 def num_to_groups(num, divisor):
@@ -144,22 +176,17 @@ def num_to_groups(num, divisor):
     return arr
 
 
-def q_sample(x_start, t, noise=None):
-
+def q_sample(scheduler: Scheduler, x_start, t, noise=None):
     if noise is None:
         noise = torch.randn_like(x_start)
 
-    sqrt_alphas_cumprod_t = extract(sqrt_alphas_cumprod, t, x_start.shape)
+    sqrt_alphas_cumprod_t = extract(scheduler.sqrt_alphas_cumprod, t, x_start.shape)
     sqrt_one_minus_alphas_cumprod_t = extract(
-        sqrt_one_minus_alphas_cumprod, t, x_start.shape
+        scheduler.sqrt_one_minus_alphas_cumprod, t, x_start.shape
     )
 
     return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
 
-@torch.no_grad()
-def sample(model, image_size, batch_size=16, channels=3):
-    res = p_sample_loop(model, shape=(batch_size, channels, image_size, image_size))
-    return res[0]
 
 
 def p_losses(denoise_model, x_start, t, noise=None, loss_type="l1"):
